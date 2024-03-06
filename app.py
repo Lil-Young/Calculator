598dc4263c8dea479f2550dcc49c6bf3ece92c84
Lil-Young <78163645+Lil-Young@users.noreply.github.com>
2024-03-06 23:43:44 +0900

    update

--- app.py
+++ app.py
@@ -13,11 +13,7 @@ if __name__ == '__main__':
         except:
             print("다시 입력해주세요")
             continue
-<<<<<<< Updated upstream
-        if operation not in ('+', '-', '*', '**', '/', '//', '%'):
-=======
         if operation not in ('+', '-', '*', '/', '//', '%'):
->>>>>>> Stashed changes
             print("다시 입력해주세요")
         elif operation == '+':
             result.add(num)
@@ -25,19 +21,12 @@ if __name__ == '__main__':
             result.sub(num)
         elif operation == '*':
             result.mul(num)
-<<<<<<< Updated upstream
         elif operation == '**':
             result.square(num)
-=======
->>>>>>> Stashed changes
         elif operation == '/':
             result.div(num)
         elif operation == '//':
             result.div_quo(num)
         elif operation == '%':
             result.div_rmd(num)
-<<<<<<< Updated upstream
             
-=======
-        
->>>>>>> Stashed changes
